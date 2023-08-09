2a440c4ba9408fbda9fe38d05791f49274628b65
elderlango <138185278+elderlango@users.noreply.github.com>
2023-08-09 16:00:24 -0600

    Delete src directory

--- src/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabaseUrl = "https://fmggwikrusxsmyiwiwqu.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtZ2d3aWtydXN4c215aXdpd3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAzMjkwNTIsImV4cCI6MjAwNTkwNTA1Mn0.l3OfJBz7N6AI2SF-9DuQicwgfNLPeIekS6qwyYt25Ng";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
