9239ca9a702c52c780e320b9a98004a20f86c35a
andy contreras <zhchypyp@gmail.com>
2023-08-10 10:27:24 -0600

    whats parcial

--- src/services/apiWhatsApp.js
+++ src/services/apiWhatsApp.js
@@ -1,1 +1,11 @@
 import supabase from "./supabase";
+
+export async function getPhone() {
+  const { data, error } = await supabase.from("productos").select("phone");
+
+  if (error) {
+    console.error(error);
+  }
+
+  return data;
+}
